4a854ab678c95f56685f99c9c3fc17dd6b1a517c
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-03-26 16:57:48 -0500

    Last commit on windows

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { of } from 'rxjs';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class PorPaisComponent {
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
 
+  constructor(private paisService: PaisService) {}
+
+  buscar = (termino: string) => {
+    this.hayError = false;
+    this.termino = termino;
+
+    of(
+      this.paisService.buscarPais(this.termino).subscribe({
+        next: (paises) => {
+          console.log(paises);
+          this.paises = paises;
+        },
+        error: (err) => {
+          this.hayError = true;
+          this.paises = [];
+        },
+      })
+    );
+  };
 }
