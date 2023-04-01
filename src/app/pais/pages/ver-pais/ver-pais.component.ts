8cbec3bf440b6e8feac02278021c6a331cc6ef4c
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-04-01 09:38:52 -0500

    Fin de la primera parte de construccion de app

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -7,26 +7,23 @@ import { Country } from '../../interfaces/pais.interface';
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VerPaisComponent implements OnInit {
-
   pais!: Country;
 
-  constructor(private activatedRoute: ActivatedRoute, private paisService: PaisService){}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) {}
   ngOnInit(): void {
-
     this.activatedRoute.params
-    .pipe(
-      switchMap(({id}) => this.paisService.getPaisPorCodigo(id)),
-      tap(console.log)
-    )
-    .subscribe(pais => {
-      this.pais = pais[0]
-    })
-
-
+      .pipe(
+        switchMap(({ id }) => this.paisService.getPaisPorCodigo(id)),
+        tap(console.log)
+      )
+      .subscribe((pais) => {
+        this.pais = pais[0];
+      });
   }
-
 }
